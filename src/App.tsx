063dd6f6501e922a15de7a2d66ca9e086c23231b
Antônio Luis <antonioluisp97@gmail.com>
2022-05-02 21:24:06 -0300

    widget funcional faltando conteudo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-interface ButtonProps {
-  text?: string;
-}
-
-function Button(props: ButtonProps){
-  return <button className="bg-violet-500 px-4 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors">{props.text ?? 'Default' }</button>
-}
-
+import { Widget } from "./components/Widget"
 
 function App() {
-  return (
-    <div className="flex gap-2">
-      <Button text="enviar"/>
-      <Button text="ok"/>
-      <Button/>
-    </div>
-  )
+  return <Widget />
 }
 
 export default App
