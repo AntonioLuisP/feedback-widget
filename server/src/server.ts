cede6f85e55dbb47c3ea8fa536174b079d9a4735
Antônio Luis <antonioluisp97@gmail.com>
2022-05-12 17:54:16 -0300

    desfazendo rota de teste no back

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,9 +8,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.get("/teste", (req, res) => {
-  return res.json({ data: "eae" })
-})
 app.listen(3333, () => {
   console.log("HTTP SERVER RUNNING")
 })
