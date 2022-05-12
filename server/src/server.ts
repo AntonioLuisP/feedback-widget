1fce13bdc2ba412b036cc70e12f6a98ede737b7c
Antônio Luis <antonioluisp97@gmail.com>
2022-05-12 17:53:30 -0300

    mobile ok

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,9 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
+app.get("/teste", (req, res) => {
+  return res.json({ data: "eae" })
+})
 app.listen(3333, () => {
   console.log("HTTP SERVER RUNNING")
 })
